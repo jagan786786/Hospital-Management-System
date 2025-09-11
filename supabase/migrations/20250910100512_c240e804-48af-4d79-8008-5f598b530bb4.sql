30b59b33ec6e378738935bd441f8b8243754a4e5
Jagannath <jagannathpatro234@gmail.com>
2025-09-11 14:42:59 +0530

    Destructuring of folder

--- supabase/migrations/20250910100512_c240e804-48af-4d79-8008-5f598b530bb4.sql
+++
@@ -1,2 +0,0 @@
--- Enable RLS on the doctors_view
-ALTER VIEW public.doctors_view SET (security_barrier=true);
--- supabase/migrations/20250910100533_798256b9-2d82-46bc-8332-127f1af210f0.sql
+++
@@ -1,3 +0,0 @@
--- Drop the problematic doctors_view to resolve security warning
--- The application can query employees table directly instead
-DROP VIEW IF EXISTS public.doctors_view;
